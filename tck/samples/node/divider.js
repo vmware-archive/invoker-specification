00869d673df17579d6faff9d2ce3e0025729a0bc
Florent Biville <fbiville@pivotal.io>
2020-03-24 18:44:56 +0100

    Update Node divider function to properly crash
    Note: this demonstrates Node invoker bug https://github.com/projectriff/node-function-invoker/issues/133

--- tck/samples/node/divider.js
+++ tck/samples/node/divider.js
@@ -1,1 +1,6 @@
-module.exports = x => 100 / x;
+module.exports = x => {
+    if (x === 0) { // would otherwise return NaN without crashing
+        throw new Error("Division by zero!");
+    }
+    return 100 / x;
+};
